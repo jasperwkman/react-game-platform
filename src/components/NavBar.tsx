81b9d76ab3dc060f28619091af44c7fe6d4649fd
Jasper.M <wk.jasper@gmail.com>
2024-06-03 18:43:46 +0100

    build color mode switch on nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,16 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import naruto from "../assets/naruto-square.png";
+import ColorModeSwitch from "./ColorModeSwitch";
 function NavBar() {
   return (
-    <HStack>
-      <Image src={naruto} boxSize="60px"></Image>
-      <Text fontSize="3xl" color="tomato">
-        Naruto
-      </Text>
+    <HStack justifyContent="space-between">
+      <div style={{ display: "flex", alignItems: "left" }}>
+        <Image src={naruto} boxSize="60px"></Image>
+        <Text fontSize="3xl" color="tomato">
+          Naruto
+        </Text>
+      </div>
+      <ColorModeSwitch />
     </HStack>
   );
 }
